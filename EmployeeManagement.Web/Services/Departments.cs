66b4609f150b209e9504c7dda845785f6813acca
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-01 21:55:05 +0530

    Added Srvices and interfaces

---
+++ EmployeeManagement.Web/Services/Departments.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Services
+{
+    public class Departments : IDepartment
+    {
+        private readonly HttpClient client;
+        public Departments(HttpClient httpClient)
+        {
+            client = httpClient;
+        }
+        public async Task<Department> GetDepartment(int id)
+        {
+            return await client.GetFromJsonAsync<Department>("api/Departments/"+id);
+        }
+
+        public async Task<List<Department>> GetDepartments()
+        {
+            return await client.GetFromJsonAsync<List<Department>>("api/Departments");
+        }
+    }
+}
---
+++ EmployeeManagement.Web/Services/IDepartment.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Services
+{
+    public interface IDepartment
+    {
+        Task<List<Department>> GetDepartments();
+        Task<Department> GetDepartment(int id);
+    }
+}
