d06c828ecdfa074aaf5f6ec5ce52b710a73be3df
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-01 21:55:47 +0530

    Added Controller for Department

---
+++ EmployeeManagement.Api/Controller/DepartmentsController.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeManagement.Api.Models;
+using EmployeeManagement.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetDepartments()
+        {
+            try
+            {
+                return Ok(await _departmentRepository.GetDepartments());
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+           
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var dep = await _departmentRepository.GetDepartment(id);
+            if (dep != null)
+            {
+                return Ok(dep);
+            }
+            else
+                return NotFound();
+        }
+    }
+}
