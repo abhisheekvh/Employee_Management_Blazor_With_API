d4b1278bce29f9a83372cc50b39bf04f5c65f695
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-01 15:30:47 +0530

    Added Edit Employee class 2 way data binding

---
+++ EmployeeManagement.Web/Pages/EditEmployeebase.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeebase :ComponentBase
+    {
+        [Inject]
+        public IEmployees emp { get; set; }
+        public Employee employee = new Employee();
+
+        [Parameter]
+        public string id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            employee = await emp.GetEmployee(int.Parse(id));
+        }
+    }
+}
