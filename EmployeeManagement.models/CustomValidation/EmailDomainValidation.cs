0471b4cdb8c4d7abaa030ccb49cdee9faff71d22
Abhishek vh <69763976+abhisheekvh@users.noreply.github.com>
2021-12-03 12:58:17 +0530

    Added class for custom validation

---
+++ EmployeeManagement.models/CustomValidation/EmailDomainValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.models.CustomValidation
+{
+    public class EmailDomainValidation :ValidationAttribute
+    {
+        public string domainName { get; set; }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] email = value.ToString().Split('@');
+            if(email[1]==domainName)
+            {
+                return null;
+            }
+            return new ValidationResult($"Domain must be {domainName}", new[] { validationContext.MemberName });
+        }
+    }
+}
